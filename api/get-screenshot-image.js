24c0ec4608d542efba9650a825507d3698d384a4
tffaruk <97652722+tffaruk@users.noreply.github.com>
2022-09-24 16:23:11 +0600

    initial commit

---
+++ api/get-screenshot-image.js
@@ -0,0 +1,74 @@
+import chromium from "chrome-aws-lambda";
+
+async function getBrowserInstance() {
+  const executablePath = await chromium.executablePath;
+
+  if (!executablePath) {
+    // running locally
+    const puppeteer = require("puppeteer");
+    return puppeteer.launch({
+      args: chromium.args,
+      headless: true,
+      defaultViewport: {
+        width: 1280,
+        height: 720,
+      },
+      ignoreHTTPSErrors: true,
+    });
+  }
+
+  return chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: {
+      width: 1280,
+      height: 720,
+    },
+    executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+}
+
+export default async (req, res) => {
+  const url = req.body.url;
+
+  // Perform URL validation
+  if (!url || !url.trim()) {
+    res.json({
+      status: "error",
+      error: "Enter a valid URL",
+    });
+
+    return;
+  }
+
+  let browser = null;
+  let result = null;
+
+  try {
+    browser = await getBrowserInstance();
+    let page = await browser.newPage();
+    await page.goto(url);
+    result = await page.screenshot({
+      path: "uploaded_on_" + Date.now() + ".jpg",
+    });
+
+    res.json({
+      status: "ok",
+      data: result,
+    });
+
+    // upload this buffer on AWS S3
+  } catch (error) {
+    console.log(error);
+    res.json({
+      status: "error",
+      data: error.message || "Something went wrong",
+    });
+    // return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+};
---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+// import Image from "next/image";
+
+export default function Home() {
+  const [websiteURL, setWebsiteURL] = useState("");
+
+  const [imageURL, setImageURL] = useState("/");
+
+  async function submitWebsiteURL() {
+    const res = await fetch("/api/get-screenshot-image", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: websiteURL,
+      }),
+    }).then((res) => res.json());
+
+    setImageURL(res.data);
+    console.log(res);
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* <Image key={imageURL} src={imageURL} width={1280} height={720} /> */}
+
+      <div className={styles.inputArea}>
+        <input
+          type="text"
+          value={websiteURL}
+          onChange={(e) => setWebsiteURL(e.target.value)}
+          placeholder="Enter a website URL"
+        />
+        <button onClick={submitWebsiteURL}>Submit URL</button>
+      </div>
+    </div>
+  );
+}
